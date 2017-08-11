21f0461cbbdf979ca999acdea9f0128c7473c097
diegosevilla <josevilla1@up.edu.ph>
2017-08-11 19:10:46 +0800

    merged landing page with server

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var connection =  mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'tiger',
+	database: 'accounts'
+});
+
+router.get("/view", function(req, res){
+	console.log(req.body.username);
+	connection.query('SELECT * from Users where username=\'diegosevilla\'' , function(err, rows, fields) {
+	   	if (!err){
+	    	console.log('The solution is: ', rows);
+	    	res.send(rows);
+	   	}
+	   	else
+	     	console.log('Error while performing Query.' + err);
+	});
 });
 
 module.exports = router;
