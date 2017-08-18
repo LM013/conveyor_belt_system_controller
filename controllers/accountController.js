15dd06fea265b9953995fe616fd62f46e585a568
gppayawal <gppayawal@gmail.com>
2017-08-18 17:15:07 +0800

    fixed sign up error handling

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -46,12 +46,12 @@ module.exports= {
         console.log(req);
         connection.query("INSERT Accounts (firstName, lastName, username, password) values (?, ?, ?, MD5(SHA1(?)))", [req.body.first_name, req.body.last_name, req.body.new_username, req.body.new_password], function(err, rows, fields){
             if(!err){
-                /*if(rows.length != 0){
-                    res.status(403).send({status: 'User already exists'});
-                }*/
                 res.status(200).send("signed up");
             } else {
                 console.log(err);
+                if(err.code == 'ER_DUP_ENTRY'){
+                    res.status(403).send({status: 'Username already exists'});
+                }
                 res.status(500).send({status: 'error'});
             }
         });
--- public/js/index.js
+++ public/js/index.js
@@ -60,7 +60,7 @@ $('#loginForm').on('submit', function(e){
 			}
 		});
 	} else {
-		Materialize.toast("Username and Password Required", 3000, 'red');
+		Materialize.toast("Username and Password Required", 3000, 'red lighten-1');
 	}
 });
 
@@ -84,8 +84,7 @@ $('#signupForm').on('submit', function(e){
 		})
 		.then((res) => {
 			switch (res.status) {
-	            case 403: message = 'User already exists'; break;
-	            case 404: message = 'Username already taken'; break;
+	            case 403: message = 'Username already taken'; break;
 	            case 500: message = 'Sign up failed. Please try again.'; break;
 	            default: message = 'Error signing up!'; break;
 	        }
@@ -96,6 +95,6 @@ $('#signupForm').on('submit', function(e){
 			}
 		});
 	} else {
-		Materialize.toast("Please fill up all fields", 3000, 'red');
+		Materialize.toast("Please fill up all fields", 3000, 'red lighten-1');
 	}
 });
