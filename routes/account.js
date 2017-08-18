1f6032a30edd7eb7c58fd86709ae805557628699
diegosevilla <josevilla1@up.edu.ph>
2017-08-18 16:50:36 +0800

    fixed transitions

--- routes/account.js
+++ routes/account.js
@@ -3,11 +3,7 @@ var router = express.Router();
 var accountController =  require('../controllers/accountController');
 
 router.post('/login', accountController.login);
-<<<<<<< HEAD
 router.post('/logout', accountController.logout);
-=======
-router.get('/logout', accountController.logout);
 router.post('/signup', accountController.signup);
->>>>>>> 84399bc27e0f409f680142574c47f5af2a4cdd9e
 
 module.exports = router;
