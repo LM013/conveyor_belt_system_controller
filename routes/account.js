9debdc2c2eb70fa91b581b8b0c17b64e11831ab8
diegosevilla <josevilla1@up.edu.ph>
2017-08-18 14:16:12 +0800

    merge

--- routes/account.js
+++ routes/account.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var accountController =  require('../controllers/accountController');
 
 router.post('/login', accountController.login);
-router.get('/logout', accountController.logout);
+router.post('/logout', accountController.logout);
 
 module.exports = router;
